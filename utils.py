65ea460d6afccd10b61bbb99c2e952de545bf494
YowKuan <yoyo6213@gmail.com>
2022-10-14 15:30:49 +0000

    added imagenet-a

--- utils.py
+++ utils.py
@@ -20,6 +20,7 @@ import math
 import cv2
 from PIL import Image
 import matplotlib.cm as cm
+import pathlib
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 ################################################################
@@ -217,7 +218,7 @@ class Contrastive_Transform:
       self.transforms = torch.nn.Sequential(
             #transforms.Resize(size=256),
             transforms.RandomResizedCrop(size=size),
-            # transforms.ColorJitter(0.8, 0.8, 0.8 , 0.2),
+            transforms.ColorJitter(0.8, 0.8, 0.8 , 0.2),
             # transforms.RandomGrayscale(p=0.2),
             # transforms.RandomRotation((-90, 90)),
             transforms.RandomHorizontalFlip(),)  
@@ -322,6 +323,50 @@ def cifar10(root):
         'test': {'data': test_set.data, 'labels': test_set.targets}
     }
 
+def load_new_test_data(version_string='', load_tinyimage_indices=False):
+    data_path = os.path.join(os.path.dirname(__file__), '../CIFAR-10.1/datasets/')
+    filename = 'cifar10.1'
+    if version_string == '':
+        version_string = 'v7'
+    if version_string in ['v4', 'v6', 'v7']:
+        filename += '_' + version_string
+    else:
+        raise ValueError('Unknown dataset version "{}".'.format(version_string))
+    label_filename = filename + '_labels.npy'
+    imagedata_filename = filename + '_data.npy'
+    label_filepath = os.path.abspath(os.path.join(data_path, label_filename))
+    imagedata_filepath = os.path.abspath(os.path.join(data_path, imagedata_filename))
+    print('Loading labels from file {}'.format(label_filepath))
+    assert pathlib.Path(label_filepath).is_file()
+    labels = np.load(label_filepath)
+    print('Loading image data from file {}'.format(imagedata_filepath))
+    assert pathlib.Path(imagedata_filepath).is_file()
+    imagedata = np.load(imagedata_filepath)
+    assert len(labels.shape) == 1
+    assert len(imagedata.shape) == 4
+    assert labels.shape[0] == imagedata.shape[0]
+    assert imagedata.shape[1] == 32
+    assert imagedata.shape[2] == 32
+    assert imagedata.shape[3] == 3
+    if version_string == 'v6' or version_string == 'v7':
+        assert labels.shape[0] == 2000
+    elif version_string == 'v4':
+        assert labels.shape[0] == 2021
+
+    if not load_tinyimage_indices:
+        return imagedata, labels
+    else:
+        ti_indices_data_path = os.path.join(os.path.dirname(__file__), '../other_data/')
+        ti_indices_filename = 'cifar10.1_' + version_string + '_ti_indices.json'
+        ti_indices_filepath = os.path.abspath(os.path.join(ti_indices_data_path, ti_indices_filename))
+        print('Loading Tiny Image indices from file {}'.format(ti_indices_filepath))
+        assert pathlib.Path(ti_indices_filepath).is_file()
+        with open(ti_indices_filepath, 'r') as f:
+            tinyimage_indices = json.load(f)
+        assert type(tinyimage_indices) is list
+        assert len(tinyimage_indices) == labels.shape[0]
+        return imagedata, labels, tinyimage_indices
+
 def imagenet1000():
 
     if 'thor' in socket.gethostname():
@@ -372,6 +417,26 @@ def load_imagenetS():
 
     if 'thor' in socket.gethostname():
         data_dir='/local/rcs/yunyun/sketch'
+    elif 'ironman' in socket.gethostname():
+        data_dir='/local/rcs/yl4888/sketch'
+    
+    imgnet_mean=(0.485, 0.456, 0.406)
+    imgnet_std=(0.229, 0.224, 0.225)
+
+    dataset = datasets.ImageFolder(
+            data_dir,
+            transforms.Compose([
+                transforms.Resize(256), transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize(imgnet_mean, imgnet_std),]))
+
+    return dataset
+
+def load_imagenetA():
+    
+    data_dir = ''
+    if 'ironman' in socket.gethostname():
+        data_dir='/local/rcs/yl4888/imagenet-a'
     
     imgnet_mean=(0.485, 0.456, 0.406)
     imgnet_std=(0.229, 0.224, 0.225)
